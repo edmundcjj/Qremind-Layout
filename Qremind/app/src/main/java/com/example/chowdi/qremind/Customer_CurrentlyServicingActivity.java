a678bbf2693cec4a5c645c2d36cb8728edbe5652
Edmund Chow <hoed92@gmail.com>
2016-03-03 15:46:08 +0800

    layout

---
+++ Qremind/app/src/main/java/com/example/chowdi/qremind/Customer_CurrentlyServicingActivity.java
@@ -0,0 +1,7 @@
+package com.example.chowdi.qremind;
+
+/**
+ * Created by chowdi on 2/3/2016.
+ */
+public class Customer_CurrentlyServicingActivity {
+}
---
+++ Qremind/app/src/main/java/com/example/chowdi/qremind/LogoCoverPageActivity.java
@@ -0,0 +1,7 @@
+package com.example.chowdi.qremind;
+
+/**
+ * Created by chowdi on 3/3/2016.
+ */
+public class LogoCoverPageActivity {
+}
