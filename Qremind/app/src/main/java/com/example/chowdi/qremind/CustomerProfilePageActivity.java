84bfc88f14cc7419e42f7b4fec8bab082c3ab30a
Winnie Lew <lewmh.winnie@yahoo.com.sg>
2016-03-05 20:58:08 +0800

    review of sprint 1 doc

--- Qremind/app/src/main/java/com/example/chowdi/qremind/CustomerProfilePageActivity.java
+++ Qremind/app/src/main/java/com/example/chowdi/qremind/CustomerProfilePageActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
--- Qremind/app/src/main/java/com/example/chowdi/qremind/GetQueueNumberActivity.java
+++ Qremind/app/src/main/java/com/example/chowdi/qremind/GetQueueNumberActivity.java
@@ -1,7 +1,30 @@
 package com.example.chowdi.qremind;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by chowdi on 2/3/2016.
  */
-public class GetQueueNumberActivity {
+public class GetQueueNumberActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_GetQueueNumberActivity);
+
+        Button NoOfPax = (Button) findViewById(R.id.bGetQNo);
+
+        NoOfPax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //codes to issue a queue number to the client based on the no. of pax entered.
+                //then send to new view.
+
+
+            }
+        });
+    }
+
+
 }
